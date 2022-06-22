13e408e32de0e79f265bd650714d7ee321e00a79
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-22 17:17:54 +0700

    feat: Subscribe push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
+const urlBase64ToUint8Array = base64String => {
+	var padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+	var base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
+
+	var rawData = window.atob(base64)
+	var outputArray = new Uint8Array(rawData.length)
+
+	for (var i = 0; i < rawData.length; ++i) {
+		outputArray[i] = rawData.charCodeAt(i)
+	}
+	return outputArray
+}
+
 const App = () => {
 	const AllowNotification = () => {
 		Notification.requestPermission().then(result => {
@@ -9,7 +22,31 @@ const App = () => {
 		})
 	}
 
-	return <button onClick={AllowNotification}>Allow Notification</button>
+	const Subscribe = () => {
+		const key =
+			'BDk-kDxLswQMajg9TJqpb9VFTjQeQmS0FE_rTVJ4f9G-v9GFkzcDt-vYkvz5dVkbCfrGmJeLTbvuNUKpOUojWB4'
+
+		global.registration.pushManager
+			.subscribe({
+				userVisibleOnly: true,
+				applicationServerKey: urlBase64ToUint8Array(key),
+			})
+			.then(pushSubscription => {
+				console.log('Subscribed!')
+
+				const subscriptionObject = JSON.stringify(pushSubscription)
+			})
+			.catch(error => {
+				console.log('Did not subscribe.', error)
+			})
+	}
+
+	return (
+		<>
+			<button onClick={AllowNotification}>Allow Notification</button>
+			<button onClick={Subscribe}>Subscribe</button>
+		</>
+	)
 }
 
 export default App
